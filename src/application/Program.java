f589f752d33eaa03bbb49e1c1c7f5cbd8e8a4788
Miqeas Pedro Mendes <miqeasmendes.com>
2020-01-05 08:11:10 +0100

    Starting to implement Class Board and Piece

--- src/application/Program.java
+++ src/application/Program.java
@@ -1,5 +1,6 @@
 package application;
 
+import boardgame.Board;
 import boardgame.Position;
 
 public class Program {
@@ -8,6 +9,8 @@ public class Program {
 		
 		Position position = new Position(5, 8);
 		System.out.println(position);
+		
+		Board board = new Board(4, 6);
 		
 
 	}
